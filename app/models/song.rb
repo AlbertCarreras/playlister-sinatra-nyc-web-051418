afa09dae483659ccc85eef1709ae4f7f59e9b16d
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-07 11:57:14 -0400

    fix library_parser

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,10 @@
 class Song < ActiveRecord::Base
-  has_many :genres
+  has_many :song_genres
   belongs_to :artist
 
-  def song_genres
-    binding.pry
-    true
-  end
+  # def song_genres
+  #   Song_Genres.all.select do |sg|
+  #     sg.song == self
+  #   end
+  # end
 end
