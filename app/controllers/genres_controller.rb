b706f156cf1b89502f9bb701cde787cf222907ef
github username <github email address>
2018-06-08 00:57:39 -0400

    finished playlister basics

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,11 @@
 class GenresController < ApplicationController
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :"genres/index"
+  end
+
+
   get '/genres/:slug' do
     @genre = Genre.find_by_slug(params["slug"])
     @songs = @genre.get_songs_from_genre
